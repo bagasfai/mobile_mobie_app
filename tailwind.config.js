28d773e9e9553c956a5432fdcd1cbccf18547be4
bagasfai <bagasfai50@gmail.com>
2025-05-26 16:21:17 +0700

    Custom | Fetch & DIsplay Movies

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,12 @@ module.exports = {
         secondary: '#151312',
         light: {
           100: '#D6C6FF',
-          200: 'A8B5DB',
-          300: '9CA4AB',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
         },
         dark: {
-          100: '221F3D',
-          200: '0F0D23',
+          100: '#221F3D',
+          200: '#0F0D23',
         },
         accent: '#AB8BFF',
 
